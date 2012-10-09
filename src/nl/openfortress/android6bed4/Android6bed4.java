680f8f12d2de2309296f7821bc69e5842cf56d62
Rick van Rein <rick@openfortress.nl>
2012-10-09 12:57:14 +0200

    Early release v0.2  - always starts the tunnel reliably  - integrates with VPN interaction (including teardown) in Android

--- src/nl/openfortress/android6bed4/Android6bed4.java
+++ src/nl/openfortress/android6bed4/Android6bed4.java
@@ -1,14 +1,20 @@
 package nl.openfortress.android6bed4;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.VpnService;
+import android.net.VpnService.Builder;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.widget.ProgressBar;
+
+import java.io.IOException;
 import java.net.*;
 
 public class Android6bed4 extends Activity {
 
-	private DatagramSocket uplink;
-	private TunnelService downlink;
+	private DatagramSocket uplink = null;
+	private TunnelService downlink = null;
 	
 	private KeepAliveService kas;
 	
@@ -42,9 +48,8 @@ public class Android6bed4 extends Activity {
 			uplink = null;
 		}
 		try {
-			downlink = new TunnelService (this, (Inet6Address) Inet6Address.getByName ("::1"));	/* TODO: Note here, not with a static IPv6 address */
+			setupTunnelService (this, (Inet6Address) Inet6Address.getByName ("::1"));	/* TODO: Note here, not with a static IPv6 address */
 		} catch (UnknownHostException uhe) {
-			downlink = null;
 		}
 		if (debug != null) debug.setProgress (20);
 		kas = new KeepAliveService (uplink, publicserver);
@@ -83,5 +88,40 @@ public class Android6bed4 extends Activity {
 		/* TODO: Nothing? */
 		if (debug != null) debug.setProgress (100);
  	}
+
+
+	/***                                                ***
+	 ***   Internal affairs -- tunnel service setup.    ***
+	 ***                                                ***/
+
+	public void setupTunnelService (Activity act, Inet6Address addr6bed4) {
+		//
+		// Prepare the context for the VPN
+		Intent mktun_intent = VpnService.prepare (act);
+		if (mktun_intent != null) {
+			// This is a new start of the VPN
+			act.startActivityForResult (mktun_intent, 0);
+		} else {
+			// Already started, apparently OK, so proceed to startup
+			this.onActivityResult (0, Activity.RESULT_OK, null);
+		}
+	}
+		
+	synchronized protected void onActivityResult (int reqcode, int resultcode, Intent data) {
+		if (resultcode == Activity.RESULT_OK) {
+			//
+			// Cleanup any prior tunnel file descriptors
+			this.onRevoke ();
+			//
+			// Setup a new tunnel
+			downlink = new TunnelService ();
+		}
+	}
+	
+	synchronized public void onRevoke () {
+		if (downlink != null) {
+			downlink.teardown ();
+		}
+	}
 
 }
--- src/nl/openfortress/android6bed4/TunnelService.java
+++ src/nl/openfortress/android6bed4/TunnelService.java
@@ -11,55 +11,9 @@ import java.io.IOException;
 
 public final class TunnelService extends VpnService {
 
-	ParcelFileDescriptor fio = null;
-
-	public TunnelService (Activity act, Inet6Address addr6bed4) {
-		//
-		// Prepare the context for the VPN
-		Intent mktun_intent = super.prepare (act);
-		if (mktun_intent != null) {
-			// This is a new start of the VPN
-			act.startActivityForResult (mktun_intent, 0);
-		} else {
-			// Already started, apparently OK, so proceed to startup
-			this.onActivityResult (0, Activity.RESULT_OK, null);
-		}
-	}
-		
-	synchronized protected void onActivityResult (int reqcode, int resultcode, Intent data) {
-		if (resultcode == Activity.RESULT_OK) {
-			//
-			// Cleanup any prior tunnel file descriptors
-			this.onRevoke ();
-			//
-			// Create a VPN builder object
-			Builder builder;
-			builder = new Builder ();
-			builder.setSession ("6bed4 uplink to IPv6");
-			builder.setMtu (1280);
-			try {
-				builder.addAddress (Inet6Address.getByName ("fe80:6:bed:4::"), 64);
-			} catch (UnknownHostException uhe) {
-				;
-			}
-			builder.addRoute ("::", 0);
-			fio = builder.establish ();
-		}
-	}
-	
-	synchronized public void onRevoke () {
-		if (fio != null) {
-			try {
-				fio.close ();
-			} catch (IOException ioe) {
-				;
-			}
-			fio = null;
-		}
-	}
+	private ParcelFileDescriptor fio = null;
 
 	public TunnelService () {
-/*NOTHERE		
 		//
 		// Create a VPN builder object
 		Builder builder;
@@ -73,7 +27,17 @@ public final class TunnelService extends VpnService {
 		}
 		builder.addRoute ("::", 0);
 		fio = builder.establish ();
-*/
+	}
+	
+	synchronized public void teardown () {
+		if (fio != null) {
+			try {
+				fio.close ();
+			} catch (IOException ioe) {
+				;
+			}
+			fio = null;
+		}
 	}
 
 }
